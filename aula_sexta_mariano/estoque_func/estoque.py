1765bd2d038199fb09ad90667c69ae4bf5b71990
vinícius Almeida Andrade <vini3104@hotmail.com>
2025-09-26 21:20:48 -0300

    Add files via upload

---
+++ aula_sexta_mariano/estoque_func/estoque.py
@@ -0,0 +1,24 @@
+"""
+1 - cadastro de produtos
+    1.1 - codigo
+    1.2 - descrição
+    1.3 - unidade(UND)
+    1.4 - preçoc de venda
+    1.5 - preço de compra médio
+    1.6 - quantidade
+    1.7 - quantidade mínima
+
+2 - movimentação
+    2.1 - codigo movimentação
+    2.1.1 - codigo produto
+    2.1.2 - tipo movimentação (entrada ou saida)
+    2.1.3 - quantidade
+    2.1.4 - valor
+    2.1.5 - sub total
+    2.2 - total
+"""
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
