c8eecc1db22acbe98634c598d3a391acf88f6bfb
vinícius Almeida Andrade <vini3104@hotmail.com>
2025-09-30 16:40:01 -0300

    feat

--- sistema_de_cadastro/routes_cliente.py
+++ sistema_de_cadastro/routes_cliente.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session, session
 from starlette import status
 from db import get_db
 from models import Cliente
@@ -9,17 +10,17 @@ cliente_router = APIRouter(prefix="/clientes", tags=["clientes"])
 
 @cliente_router.post("/cadastrar", status_code=status.HTTP_201_CREATED)#, response_model=)
 async def cadastrar_cliente( payload : Cliente, db: AsyncSession = Depends(get_db)):
-    novo = Cliente(
+    novo_cliente = Cliente(
         nome=payload.nome.strip(),
         email=payload.email.strip(),
         telefone=payload.telefone.strip()
 
     )
 
-    db.add(novo)
+    db.add(novo_cliente)
     await db.commit()
-    await db.refresh(novo)
-    return novo
+    await db.refresh(novo_cliente)
+    return {"mensage":f"Cliente {novo_cliente.nome} de id:[{novo_cliente.id}] cadastrado com sucesso!"}
 
 @cliente_router.get("/", status_code=status.HTTP_200_OK)
 async def listar_clientes(db: AsyncSession = Depends(get_db)):
@@ -29,7 +30,7 @@ async def listar_clientes(db: AsyncSession = Depends(get_db)):
 
     lista_clientes = resultado.scalars().all()
     if not lista_clientes:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Nenhum cliente cadastrado")
     return lista_clientes
 
 @cliente_router.patch("/{id}", status_code=status.HTTP_200_OK)
@@ -38,3 +39,10 @@ async def atualizar_cliente(id: int, db: AsyncSession = Depends(get_db)):
 
 @cliente_router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def deletar_cliente(id: int, db: AsyncSession = Depends(get_db)):
+    del_cliente = db.query(Cliente).filter(Cliente.id == id).scalar()
+    if not del_cliente:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Cliente não encontrado")
+    await db.delete(del_cliente)
+    await db.commit()
+
+    return None
